c403b176a15dd8de571c8f117b6136fe511e606a
Matthew Healy <matthew@liamhealy.xyz>
2023-11-21 23:47:03 +0100

    Remove failing test

--- test/test_ruby_rewriter.rb
+++ test/test_ruby_rewriter.rb
@@ -3,11 +3,7 @@
 require "test_helper"
 
 class TestRubyRewriter < Minitest::Test
-  def test_that_it_has_a_version_number
+  def test_version_number_exists
     refute_nil ::RubyRewriter::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
