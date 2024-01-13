d3fa1d95026659016b43e8dbf707a47f322a92bc
Matthew Healy <matthew@liamhealy.xyz>
2024-01-13 23:53:03 +0100

    First commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+
+  description = "An untitled programming language project.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        preCommitHook = pre-commit-hooks.lib.${system}.run {
+          src = self;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${preCommitHook.shellHook}
+          '';
+
+          nativeBuildInputs = with pkgs; [
+            rustc
+            rustfmt
+            cargo
+            clippy
+            rust-analyzer
+            nixpkgs-fmt
+          ];
+        };
+
+        checks = {
+          pre-commit = preCommitHook;
+        };
+      }
+    );
+}
